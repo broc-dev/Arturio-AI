3d23f67e739c134b93327382fd1542d442c2a4a0
Broc <broc@brocsmith.com>
2022-07-21 13:13:58 -0700

    Initial

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,13 +1,25 @@
-import { useQuery } from "@apollo/client";
 import { Contract } from "@ethersproject/contracts";
 import { shortenAddress, useCall, useEthers, useLookupAddress } from "@usedapp/core";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
-import { Body, Button, Container, Header, Image, Link } from "./components";
-import logo from "./ethereumLogo.png";
+import { Body, Button, Container, Image, Link,
+  NavBar, LogoSection, NavLogo, NavWordmark, NavItems, NavLink, Logo,
+  Heading, HeadingTwo, ContentSection, Subtext,
+  HeroSection, HeroContent, CTAButtons, CenterImage, HeroImage, SampleImage,
+  InfoSection,
+  MintSection, MintInterface, InputText, InputInfo, MintButton, MintBox, MintButtonContainer,
+  MintAmountSelectorContainer, MintAmountSelectorButton,
+  AboutSection, AboutItem,
+  FooterSection, FooterLinks, FooterLink, FooterTitle
+} from "./components";
+
+import logo from "./logo.svg";
+import sampleOne from "./art-sample.png";
+import sampleTwo from "./curated.png";
+
+import logoDesign from "./logo-design.png";
 
 import { addresses, abis } from "@my-app/contracts";
-import GET_TRANSFERS from "./graphql/subgraph";
 
 function WalletButton() {
   const [rendered, setRendered] = useState("");
@@ -32,7 +44,8 @@ function WalletButton() {
   }, [error]);
 
   return (
-    <Button
+    <Button buttonType="secondary"
+      style={{"marginLeft": "20px"}}
       onClick={() => {
         if (!account) {
           activateBrowserWallet();
@@ -47,42 +60,171 @@ function WalletButton() {
   );
 }
 
-function App() {
-  // Read more about useDapp on https://usedapp.io/
-  const { error: contractCallError, value: tokenBalance } =
-    useCall({
-       contract: new Contract(addresses.ceaErc20, abis.erc20),
-       method: "balanceOf",
-       args: ["0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C"],
-    }) ?? {};
+function MintingPanel() {
+  const [amountToMint, setAmount] = useState(10);
+  const [mintStatus, setMintStatus] = useState("closed");
+  const selOne = useRef(null);
+  const selFive = useRef(null);
+  const selTen = useRef(null);
+  const HIGHLIGHT_COLOR = "#E0D517";
 
-  const { loading, error: subgraphQueryError, data } = useQuery(GET_TRANSFERS);
+  /**
+   * Checks the connected contract for the current mint status, then updates mintStatus.
+   */
+  useEffect(() => {
+    if(mintStatus === "closed") return
+    if(amountToMint == 1) {
+      selOne.current.style.borderColor = HIGHLIGHT_COLOR;
+
+      selFive.current.style.borderColor = "#494949";
+      selTen.current.style.borderColor = "#494949";
+    } else if (amountToMint == 5) {
+      selFive.current.style.borderColor = HIGHLIGHT_COLOR;
+
+      selOne.current.style.borderColor = "#494949";
+      selTen.current.style.borderColor = "#494949";
+    } else if (amountToMint == 10) {
+      selTen.current.style.borderColor = HIGHLIGHT_COLOR;
+      
+      selOne.current.style.borderColor = "#494949";
+      selFive.current.style.borderColor = "#494949";
+    }
+  }, [amountToMint]);
 
   useEffect(() => {
-    if (subgraphQueryError) {
-      console.error("Error while querying subgraph:", subgraphQueryError.message);
-      return;
-    }
-    if (!loading && data && data.transfers) {
-      console.log({ transfers: data.transfers });
-    }
-  }, [loading, subgraphQueryError, data]);
+    
+  }, []);
+
+  if(mintStatus === "closed") {
+    return (
+      <MintBox>
+        <Subtext style={{"fontSize": "48px", "lineHeight": "54px", "margin": "0", "color": "#aa5555"}}>MINT<br />NOT YET<br />ENABLED</Subtext>
+      </MintBox>
+    )
+  }
+  return (
+    <MintBox>
+      <Subtext>
+        1000 Pieces<br />
+        10 Max Mint<br />
+        1 Free Mint<br />
+        0.1 ETH Each<br />
+        </Subtext>
+        <InputInfo>
+        Amount to mint: <span style={{"color": "#78715E"}}>{amountToMint}</span>
+        </InputInfo>
+        <MintAmountSelectorContainer>
+          <MintAmountSelectorButton ref={selOne} onClick={() => setAmount(1)}>1</MintAmountSelectorButton>
+          <MintAmountSelectorButton ref={selFive} onClick={() => setAmount(5)}>5</MintAmountSelectorButton>
+          <MintAmountSelectorButton ref={selTen} onClick={() => setAmount(10)}>10</MintAmountSelectorButton>
+        </MintAmountSelectorContainer>
+        {/* <InputText {...inputProps} placeholder="10" type='number' min={1} max={10} step={1} /> */}
+        <MintButtonContainer>
+          <MintButton>Mint</MintButton>
+        </MintButtonContainer>
+      </MintBox>
+  )
+}
+
+function App() {
+  // Read more about useDapp on https://usedapp.io/
+  // const { error: contractCallError, value: tokenBalance } =
+  //   useCall({
+  //      contract: new Contract(addresses.ceaErc20, abis.erc20),
+  //      method: "balanceOf",
+  //      args: ["0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C"],
+  //   }) ?? {};
 
   return (
     <Container>
-      <Header>
-        <WalletButton />
-      </Header>
+      <NavBar>
+        <LogoSection>
+          <NavLogo src={logo} alt="ethereum-logo" />
+          <NavWordmark>Arturio</NavWordmark>
+        </LogoSection>
+        <NavItems>
+          <NavLink href="#">Home</NavLink>
+          <NavLink href="#mint">Mint</NavLink>
+          <NavLink href="https://opensea.com/collection/ArturioAI">OpenSea</NavLink>
+          <NavLink href="https://twitter.com/arturioai">Twitter</NavLink>
+          <WalletButton />
+        </NavItems>
+      </NavBar>
       <Body>
-        <Image src={logo} alt="ethereum-logo" />
-        <p>
-          Edit <code>packages/react-app/src/App.js</code> and save to reload.
-        </p>
-        <Link href="https://reactjs.org">
-          Learn React
-        </Link>
-        <Link href="https://usedapp.io/">Learn useDapp</Link>
-        <Link href="https://thegraph.com/docs/quick-start">Learn The Graph</Link>
+        <HeroSection>
+          <HeroContent>
+            <h1>THE NEXT<br />GENERATION<br />OF ART</h1>
+            <h2>"SEE THE WORLD THROUGH NEW EYES WITH AI GENERATED ART"</h2>
+            <CTAButtons>
+              <Link href="#mint" style={{"marginRight": "20px"}}>
+                <Button buttonType="primary">
+                  Mint
+                </Button>
+              </Link>
+              <Link href="#">
+                <Button buttonType="secondary">
+                  OpenSea
+                </Button>
+                </Link>
+            </CTAButtons>
+          </HeroContent>
+          <HeroImage>
+            <SampleImage src={sampleOne} alt="sample-one" />
+          </HeroImage>
+        </HeroSection>
+        <InfoSection>
+          <ContentSection>
+            <Heading>FROM TOP<br />TO BOTTOM</Heading>
+            <Subtext>The art, branding, website design for Arturio were created from the power of DALL-E 2, GPT-3, MidJourney, and CoPilot, with minimal help from human programmers.
+              <br /><span style={{"color": "#A87A34", "display": "block","marginTop": "20px"}}>The future is now.</span>
+              </Subtext>
+          </ContentSection>
+          <CenterImage style={{"marginLeft": "40px"}}>
+            <SampleImage src={sampleTwo} alt="sample-two" />
+          </CenterImage>
+        </InfoSection>
+        <MintSection id="mint">
+          <MintInterface>
+            <Heading>MINT<br />YOUR<br />ARTURIO</Heading>
+            <MintingPanel />
+          </MintInterface>
+        </MintSection>
+        <AboutSection id="mint">
+          <Heading>ABOUT<br />ARTURIO</Heading>
+          <Subtext>Arturio was developed from start to finish using proprietary artificial technology. For each of the below, AI played an outsized role in their creation.</Subtext>
+          <AboutItem>
+            <div>
+              <HeadingTwo>LOGO DESIGN</HeadingTwo>
+              <Subtext>Arturio was given a prompt of "Clean lines logo mark using white and yellow ochre for an AI art project" to come up with the current design of the logo</Subtext>
+            </div>
+            <Image src={logoDesign} />
+          </AboutItem>
+          <AboutItem>
+            <div>
+              <HeadingTwo>WEB DESIGN</HeadingTwo>
+              <Subtext>Arturio was given a prompt of "Clean lines logo mark using white and yellow ochre for an AI art project" to come up with the current design of the logo</Subtext>
+            </div>
+            {/* <Image src={webDesign} /> */}
+          </AboutItem>
+        </AboutSection>
+        <FooterSection>
+          <Image src={logo} alt="arturio-logo" style={{"maxHeight": "70%"}} />
+          <FooterLinks>
+            <FooterTitle>GENERAL</FooterTitle>
+            <FooterLink href="#">Home</FooterLink>
+            <FooterLink href="#mint">Mint</FooterLink>
+            <FooterLink href="https://opensea.com/collection/ArturioAI">OpenSea</FooterLink>
+            <FooterLink href="https://twitter.com/arturioai">Twitter</FooterLink>
+          </FooterLinks>
+          <FooterLinks>
+            <FooterTitle>INFORMATION</FooterTitle>
+            <FooterLink href="#about">About</FooterLink>
+            <FooterLink href="#">Contact</FooterLink>
+            <FooterLink href="#">GitHub</FooterLink>
+            <FooterLink href="#">Privacy Policy</FooterLink>
+            <FooterLink href="#">Terms of Use</FooterLink>
+          </FooterLinks>
+        </FooterSection>
       </Body>
     </Container>
   );
--- packages/react-app/src/components/index.js
+++ packages/react-app/src/components/index.js
@@ -1,53 +1,460 @@
 import styled from "styled-components";
+import heroBg from "../hero-bg.png";
+import mintBg from "../mint-bg.png";
+import buttonBg from "../button-bg.png";
+
+export const NavBar = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  background-color: #000000;
+  max-height: 70px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  padding: 20px 80px;
+  border-bottom: 1px solid #494949;
+  overflow: hidden;
+  color: #ffffff;
+  z-index: 2;
+`;
+
+export const NavItems = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem 1rem;
+`;
+
+export const NavLink = styled.a`
+  color: #ffffff;
+  text-decoration: none;
+  font-size: 1.2rem;
+  font-weight: 400;
+  padding: 0.5rem 1.5rem;
+  transition: all 100ms ease;;
+  &:hover {
+    color: #E0D517;
+  }
+`;
 
 export const Body = styled.div`
+  padding-top: 50px;
+  width: 100%;
   align-items: center;
   color: white;
   display: flex;
   flex-direction: column;
   font-size: calc(10px + 2vmin);
   justify-content: center;
-  margin-top: 40px;
+`;
+
+export const HeroSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 90vh;
+  padding: 0 80px;
+  
+  &::before {
+    content: "";
+    background-image: url(${heroBg});
+    background-size: cover;
+    background-position: center;
+    opacity: 40%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -2;
+  }
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 60%;
+    background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.8));
+    z-index: -1
+  };
+`;
+
+export const HeroContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  width: 50%;
+  height: 100%;
+  h1 {
+    font-size: 78px;
+    line-height: 82px;
+    font-weight: 700;
+    margin-bottom: 0.3rem;
+  }
+  h2 {
+    font-size: 1rem;
+    line-height: 1.8rem;
+    font-weight: 400;
+    max-width: 500px;
+    margin-bottom: 1rem;
+    color: #78715E;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+    h1 {
+      font-size: 2rem;
+    }
+    h2 {
+      font-size: 1rem;
+    }
+  }
+`;
+
+export const HeroImage = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+}`;
+
+export const CenterImage = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}`;
+
+export const SampleImage = styled.img`
+  width: 500px;
+  box-shadow: 0 0 14px rgba(0, 0, 0, 0.3);
+  transition: all 700ms ease;
+  &:hover {
+    transform: scale(1.03);
+  }
+`;
+
+export const CTAButtons = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+`;
+
+export const InfoSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 90vh;
+  padding: 20px 80px;
+`;
+
+export const Heading = styled.h1`
+  color: #cccccc;
+  font-size: 58px;
+  line-height: 72px;
+  font-weight: 600;
+  margin-bottom: 0.3rem;
+  @media (max-width: 900px) {
+    font-size: 4rem;
+  }
+`;
+
+export const HeadingTwo = styled.h2`
+  color: #aaaaee;
+  font-size: 2.4rem;
+  line-height: 3.2rem;
+  font-weight: 500;
+  max-width: 500px;
+  margin-top: 20px;
+  margin-bottom: 0.8rem;
+}`;
+
+export const Subtext = styled.h2`
+  color: #78715E;
+  max-width: 500px;
+  font-size: 1rem;
+  line-height: 22px;
+  font-weight: 400;
+  margin-bottom: 1rem;
+  @media (max-width: 900px) {
+    font-size: 1rem;
+  }
+`;
+
+export const MintSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 90vh;
+  padding: 20px 80px;
+  position: relative;
+
+  &::before {
+    content: "";
+    background-image: url(${mintBg});
+    background-size: cover;
+    background-position: center;
+    opacity: 40%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -2;
+  }
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    top: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8));
+    z-index: -1
+  };
+`;
+
+export const MintInterface = styled.div`
+  background-color: #000000;
+  box-shadow: 0 0 14px rgba(0, 0, 0, 0.5);
+  padding: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const InputInfo = styled.p`
+  width: 100%;
+  margin-bottom: 0;
+  color: #494949;
+  font-size: 0.8rem;
+  line-height: 1.8rem;
+  font-weight: 500;
+`;
+
+export const InputText = styled.input`
+  width:  100%;
+  height: 40px;
+  border: 1px solid #494949;
+  background-color: #000000;
+  color: #cccccc;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  font-weight: 400;
+  margin-bottom: 1rem;
+  padding: 0.5rem 1rem;
+  transition: all 100ms ease;
+  &:hover {
+    background-color: #111111;
+  }
+  &:focus {
+    border: 1px solid #E0D517;
+    outline: none;
+  }
 `;
 
 export const Button = styled.button`
-  background-color: white;
-  border: none;
-  border-radius: 8px;
-  color: #282c34;
+  background-color: #000000;
+  border: 1px solid ${props => props.buttonType == "primary" ? "#ffffff" : "#cccccc"};
+  height: 50px;
+  color: ${props => props.buttonType == "primary" ? "#ffffff" : "#cccccc"};
   cursor: pointer;
-  font-size: 16px;
-  margin: 0px 20px;
+  font-family: 'IBM Plex Mono', monospace;
+  font-size: 18px;
+  font-weight: 400;
+  margin: 0;
+  padding: 12px 30px;
+  min-width: 140px;
+  text-align: center;
+  text-decoration: none;
+  transition: all 150ms;
+  &:hover {
+    background-color: ${props => props.buttonType == "primary" ? "#E0D517" : "#e1e1e1"};
+    color: #000000;
+    border: 1px solid rgba(0,0,0,0);
+  }
+`;
+
+export const MintBox = styled.div`
+  background-color: #111111;
+  width: 340px;
+  padding: 40px;
+  margin-left: 80px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}`;
+
+export const MintAmountSelectorContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+}`;
+
+export const MintAmountSelectorButton = styled.button`
+  background-color: #000000;
+  border: 1px solid #494949;
+  height: 50px;
+  color: #cccccc;
+  cursor: pointer;
+  font-family: 'IBM Plex Mono', monospace;
+  font-size: 18px;
+  font-weight: 400;
+  width: 30%;
+  margin: 0;
+  padding: 12px 30px;
+  transition: all 150ms;
+}`;
+
+export const MintButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-top: 14px;
+  background-size: 200% 200%;
+  background-image: url(${buttonBg});
+  background-position: center;
+`;
+
+export const MintButton = styled.button`
+  background-color: rgba(0,0,0,0.5);
+  width: 100%;
+  overflow: hidden;
+  border: 1px solid #494949;
+  height: 50px;
+  color: #ffffff;
+  cursor: pointer;
+  font-family: 'IBM Plex Mono', monospace;
+  font-weight: 400;
+  font-size: 18px;
+  margin: 0;
   padding: 12px 24px;
   text-align: center;
   text-decoration: none;
+  transition: all 150ms;
+  position: relative;
+  &:hover {
+    background-color: rgba(0,0,0,0.1);
+    border: 1px solid #E0D517;
+  }
 `;
 
 export const Container = styled.div`
-  background-color: #282c34;
   display: flex;
   flex-direction: column;
   height: calc(100vh);
 `;
 
-export const Header = styled.header`
-  align-items: center;
-  background-color: #282c34;
-  color: white;
+export const ContentSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  width: 100%;
+}`;
+
+export const AboutSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  width: 100%;
+  padding: 40px 80px;
+}`;
+
+export const AboutItem = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: flex-end;
-  min-height: 70px;
+  justify-content: space-between;
+  background-color: #111111;
+  width: 100%;
+  padding: 40px;
+  margin: 40px 0 0 0;
+}`;
+
+export const FooterSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: flex-start;
+  width: 100%;
+  height: 240px;
+  padding: 20px 80px;
+  background-color: #111111;
+  color: #ffffff;
+  font-size: 0.8rem;
+  line-height: 1.8rem;
+  font-weight: 500;
+}`;
+
+export const FooterLinks = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+}`;
+
+export const FooterTitle = styled.p`
+  display: block;
+  width: 100%;
+  font-size: 1.2rem;
+  font-weight: 500;
+  margin-bottom: 0;
+  color: #ffffff;
+  border-bottom: 1px solid #E0D517;
+`;
+
+export const FooterLink = styled.a`
+  color: #78715E;
+  text-decoration: none;
+  &:hover {
+    color: #E0D517;
+    text-decoration: underline;
+  }
+}`;
+
+export const LogoSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const NavWordmark = styled.p`
+  font-size: 1.5rem;
+  font-weight: bold;
+  font-style: italic;
+  `;
+
+export const NavLogo = styled.img`
+  height: 50px;
+  margin-right: 10px;
 `;
 
 export const Image = styled.img`
-  height: 40vmin;
-  margin-bottom: 16px;
+  height: 100%;
+  max-width: 30vw;
+  max-height: 30vh;
   pointer-events: none;
 `;
 
 export const Link = styled.a.attrs({
-  target: "_blank",
   rel: "noopener noreferrer",
 })`
   color: #61dafb;
